b515ae6b62702025c43fff9ae7c1a46853a53682
Jakub Wilk <jwilk@jwilk.net>
2017-06-18 16:26:51 +0200

    Initial import.

---
+++ Makefile
@@ -0,0 +1,27 @@
+# Copyright © 2017 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+INSTALL = install
+CC = gcc
+CFLAGS = -W -Wall -O2 -g
+
+PREFIX = /usr/local
+DESTDIR =
+
+libdir = $(PREFIX)/lib
+
+.PHONY: all
+all: libgetenvy.so
+
+.PHONY: install
+install: libgetenvy.so
+	install -D -m 644 $(<) $(DESTDIR)$(libdir)/$(<)
+
+.PHONY: clean
+clean:
+	rm -f *.so
+
+lib%.so: %.c
+	$(CC) $(CFLAGS) $(CPPFLAGS) -shared -fPIC -ldl $(<) -o $(@)
+
+# vim:ts=4 sts=4 sw=4 noet
---
+++ getenvy.c
@@ -0,0 +1,41 @@
+/* Copyright © 2017 Jakub Wilk <jwilk@jwilk.net> 
+ * SPDX-License-Identifier: MIT
+ */
+
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <stdio.h>
+
+static int verbose = 0;
+static char * (*orig_getenv)(const char *name);
+
+char *getenv(const char *name)
+{
+    if (!orig_getenv) {
+        orig_getenv = dlsym(RTLD_NEXT, "getenv");
+        if (orig_getenv)
+            verbose = !!orig_getenv("GETENVY");
+        else {
+            fprintf(stderr, "getenvy: cannot acquire original getenv()\n");
+            return NULL;
+        }
+    }
+    if (verbose) {
+        const char *p;
+        fprintf(stderr, "getenv(\"");
+        for (p=name; *p; p++) {
+            unsigned char c = *p;
+            if (c == '\\' || c == '"')
+                fprintf(stderr, "\\%c", c);
+            else if (c < ' ' || c >= 0x7F)
+                fprintf(stderr, "\\x%02x", c);
+            else
+                fprintf(stderr, "%c", c);
+        }
+        fprintf(stderr, "\")\n");
+    }
+    return orig_getenv(name);
+}
+
+/* vim:set ts=4 sts=4 sw=4 et:*/
